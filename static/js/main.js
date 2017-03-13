d39c0b149a1ef5b215357c1c437f9dc6a10850cd
Robert Gaspar <gasparrobi@gmail.com>
2017-03-13 18:52:35 +0100

    adding cards to the list and kind of dragging implemented

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,34 @@
 
 $( document ).ready(function() {
 
+	
 	var card = "<div class='card'><div class='card_header'><p id='todo'>To do</p></div><div class='card_content' id='card_content'></div> <p class='add_task' id='add_task'>add task</p></div>";
 
 	$(document.body).on("click", ".add_task", function(e){
 		var $x = $(e.target);
 		console.log($x.parent());
-		//$("#card_content").append("<div class='task'></div>");
+		$list = $x.prev();
+		console.log($list);
+		$list.append("<div class='task'>" + Math.floor((Math.random() * 10) + 1) + "</div>");
 	})
 
 
 	$(".create_new").click(function(){
 		$(".row").append(card);
+		//save();
 	})
 
+	dragula([document.querySelector('.card_content')], { staticClass: 'static', animation: 300 });
+	
+
+	/*
+	var cards = []
+	function save(){
+		$(".card").each(function(){
+			cards.push($(this).html());
+			console.log(cards);
+		});
+	}
+	*/
 });
+
