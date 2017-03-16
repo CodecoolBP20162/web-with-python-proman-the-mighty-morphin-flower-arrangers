c274dcdf95b195b09f4ab20f03db802be757e104
Robert Gaspar <gasparrobi@gmail.com>
2017-03-16 07:55:33 +0100

    lots of css, dragging done, saving is 95% done;

--- static/js/main.js
+++ static/js/main.js
@@ -4,51 +4,53 @@ $( document ).ready(function() {
 	
 	var card = "<div class='card'><div class='card_header'><p class='title'>To do</p></div><div class='card_content' id='card_content'></div> <p class='add_task' id='add_task'>add task</p></div>";
 
+	// ADD NEW CARD TO LIST
 	$(document.body).on("click", ".add_task", function(e){
 		var $x = $(e.target);
-		//console.log($x.parent());
 		$list = $x.prev();
-		//console.log($list);
 		var new_item = "<div class='task'> <p contenteditable='true'; onclick='$(this).focus();'>task " + Math.floor((Math.random() * 10) + 1) + "</p></div>"
 		$list.append(new_item);
 		save_cards();
 		
 	})
 
-
+	// ADD NEW LIST TO BOARD
 	$(".create_new").click(function(){
 		$(".row").append(card);
-		//save();
+		save_cards();
 	})
 
-	dragula([document.querySelector('.card_content')], { staticClass: 'static', animation: 300 })
-	.on('drop', function(){
-		save_cards();
-	});
-	
+	// MAKE CARDS DRAGGABLE
 
-	/*
-	var cards = []
-	function save(){
-		$(".card").each(function(){
-			cards.push($(this).html());
-			console.log(cards);
-		});
-	}
-	*/
+	var drake = dragula({
+		isContainer: function (el) {
+			return el.classList.contains('card_content');
+		}}).on('drop', function(){
+				save_cards();
+			});
 
+	var drake2 = dragula({
+		isContainer: function (el) {
+			return el.classList.contains('row');
+		},
+		moves: function (el, container, handle) {
+			return handle.classList.contains('handle');
+		}}).on('drop', function(){
+				save_cards();
+			});
+
+	// dragula([document.querySelector('.card_content')], { staticClass: 'static', animation: 300 })
+	// .on('drop', function(){
+	// 	save_cards();
+	// });
+
+	// CARD OBJECT
 	function proman_list(title, cards){
 		this.title = title;
 		this.cards = cards;
-
-		this.save_as_json = function(name){
-			localStorage.setItem(name, JSON.stringify(this));
-			ListNames.push(name);
-			console.log(ListNames);
-		}
 	}
 
-
+	// SAVING ALL INFORMATION TO LOCALSTORAGE
 	function save_cards(){
 
 		var obj_list = [];
@@ -62,6 +64,7 @@ $( document ).ready(function() {
 
 			var card_obj = new proman_list(title, cards);
 			obj_list.push(card_obj);
+			// console.log(title);
 			
 		});
 
@@ -70,7 +73,10 @@ $( document ).ready(function() {
 		console.log(x);
 	}
 
-
+	// SAVING TO LOCALSTORAGE AFTER EDITING
+	$("p").focusout(function(){
+		save_cards();
+	})
 
 
 
