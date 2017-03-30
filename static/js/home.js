17e02eb4d590612860f41d05a850f6733ec7cf53
Robert Gaspar <gasparrobi@gmail.com>
2017-03-30 13:32:39 +0200

    deploying to heroku

--- static/js/home.js
+++ static/js/home.js
@@ -88,6 +88,24 @@ jQuery( document ).ready(function($) {
         request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
         request.send(JSON.stringify(data));
     }
+
+
+    $.fn.selectText = function(){
+        var doc = document;
+        var element = this[0];
+        //console.log(this, element);
+        if (doc.body.createTextRange) {
+            var range = document.body.createTextRange();
+            range.moveToElementText(element);
+            range.select();
+        } else if (window.getSelection) {
+            var selection = window.getSelection();        
+            var range = document.createRange();
+            range.selectNodeContents(element);
+            selection.removeAllRanges();
+            selection.addRange(range);
+    }
+};
 })   
 
 
